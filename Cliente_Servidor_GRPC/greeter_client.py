f3c49d2490ee1fda89769dda23fe7e72981e547b
Thalia <thalia_sara@hotmail.com>
2021-09-06 15:32:10 -0300

    Server

--- Cliente_Servidor_GRPC/greeter_client.py
+++ Cliente_Servidor_GRPC/greeter_client.py
@@ -15,7 +15,7 @@ senha = input("Senha: ")
 def run():
     channel = grpc.insecure_channel('localhost:50051')
     stub = gerenciar_adm_pb2_grpc.UserStub(channel)
-    response = stub.login(gerenciar_adm_pb2.LoginRequest(usuario=usuario, senha=senha))
+    response = stub.login(gerenciar_adm_pb2.LoginRequest(usuario = usuario, senha = senha))
     if (response.responseMessage == "Usuário logado"):
         print(f"\nBem vind@, {usuario}.")
         print("Está tudo preparado para você cadastrar um novo cliente!\n")
@@ -23,8 +23,8 @@ def run():
             usuario_s = input("Nome de login do cliente: ")
             dados_cadastro[usuario_s] = input("Senha do cliente: ")
 
-            stub.cadastro(gerenciar_adm_pb2.CadastroRequest(usuario_s=usuario_s, senha_s=dados_cadastro[usuario_s]))
-            if (gerenciar_adm_pb2.CadastroResponse(responseMessage_s='Usuário Cadastrado')):
+            stub.cadastro(gerenciar_adm_pb2.CadastroRequest(usuario_s = usuario_s, senha_s = dados_cadastro[usuario_s]))
+            if (gerenciar_adm_pb2.CadastroResponse(responseMessage_s = 'Usuário Cadastrado')):
                 print("\nUsuário cadastrado com sucesso no sistema.\n")
             novoCadastro = int(input("Deseja cadastrar um novo usuário? \n 1 - Sim \n 0 - Não\n"))
 
